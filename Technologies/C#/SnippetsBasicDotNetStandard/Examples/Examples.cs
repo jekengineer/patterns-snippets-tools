8c390145527f2a101c6d4eaa5cff70eb0b975298
James Kennedy <jekengineer@gmail.com>
2019-05-04 04:26:10 -0400

    Created Calculate Sume of Numbers function, Created Tests, tried to run tests and it crashed. Now I nee dto finish making the tests then make an example usage of Calculate Sum of Numbers.

---
+++ Technologies/C#/SnippetsBasicDotNetStandard/Examples/Examples.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examples
+{
+    class Examples
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Technologies/C#/SnippetsBasicDotNetStandard/SnippetsBasicDotNetStandard/CalculateSumOfNumbers.cs
+++ Technologies/C#/SnippetsBasicDotNetStandard/SnippetsBasicDotNetStandard/CalculateSumOfNumbers.cs
@@ -1,8 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnippetsBasicDotNetStandard
 {
     public class CalculateSumOfNumbers
     {
+        // Given three numbers A, B, C you have to write a function named calcSum() which takes these 3 numbers as arguments and returns their sum.
+
+        // given two or more numbers
+
+        public long Calculate(List<long> numbersToAdd)
+        {
+            return Addition(numbersToAdd);
+        }
+
+        public long Calculate(long[] numbersToAdd)
+        {
+            List<long> numbersToAddConversionList = new List<long>();
+            numbersToAddConversionList.AddRange(numbersToAdd);
+            return Addition(numbersToAddConversionList);
+        }
+
+
+        protected long Addition(List<long> numbersToAdd)
+        {
+            long sumOfNumbers = 0;
+            for(int i = 0; i < numbersToAdd.Count; i++) {
+                sumOfNumbers += numbersToAdd[i];
+            }
+
+            return sumOfNumbers;
+        }
+
+
+
+
+        public long AdditionTest(List<long> numbersToAdd)
+        {
+            return Addition(numbersToAdd);
+        }
+
+        public long AdditionTest(long[] numbersToAdd)
+        {
+            List<long> numbersToAddConversionList = new List<long>();
+            numbersToAddConversionList.AddRange(numbersToAdd);
+            return Addition(numbersToAddConversionList);
+        }
+
+        
+
     }
 }
