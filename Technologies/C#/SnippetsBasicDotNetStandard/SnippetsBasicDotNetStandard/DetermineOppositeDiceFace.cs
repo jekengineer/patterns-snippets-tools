d57cbc5fe93c6f11e491cec0a5df65203c449274
James Kennedy <jekengineer@gmail.com>
2019-05-05 23:28:17 -0400

    Added new problem, determine opposite dice face

---
+++ Technologies/C#/SnippetsBasicDotNetStandard/SnippetsBasicDotNetStandard/DetermineOppositeDiceFace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnippetsBasicDotNetStandard
+{
+    public class DetermineOppositeDiceFace
+    {
+
+        protected int DetermineOpposingDiceFace(int visibleDiceFace)
+        {
+            int oppositeDiceFace = 0;
+
+
+
+            return oppositeDiceFace;
+        }
+
+
+    }
+}
